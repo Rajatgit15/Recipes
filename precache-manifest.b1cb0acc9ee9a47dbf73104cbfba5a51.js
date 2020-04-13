8780c0cac1599dd5119321c1651fdc74c4c3cbf6
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-04-14 00:22:51 +0530

    Updates

---
+++ precache-manifest.b1cb0acc9ee9a47dbf73104cbfba5a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d0c7ae35d12d108b1dc352573c95e7",
+    "url": "/Recipes/index.html"
+  },
+  {
+    "revision": "83a66364442888bb4f3c",
+    "url": "/Recipes/static/css/main.5bb39325.chunk.css"
+  },
+  {
+    "revision": "913d38d89588f2783321",
+    "url": "/Recipes/static/js/2.f4757969.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Recipes/static/js/2.f4757969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a66364442888bb4f3c",
+    "url": "/Recipes/static/js/main.123c6055.chunk.js"
+  },
+  {
+    "revision": "109a7ada1e95da863810",
+    "url": "/Recipes/static/js/runtime-main.8944404b.js"
+  }
+]);
